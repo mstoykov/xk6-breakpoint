39a846b937fc7547a77bbc5e01e9ba1563b226cd
Mihail Stoykov <M.Stoikov@gmail.com>
2021-06-07 18:01:45 +0300

    Print stack trace when breaking

--- breakpoint.go
+++ breakpoint.go
@@ -4,6 +4,7 @@ package breakpoint
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"os"
@@ -23,7 +24,17 @@ type breakpoint struct{}
 func (_ *breakpoint) Break(ctx context.Context, o *goja.Object) {
 	rt := common.GetRuntime(ctx)
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("breakpoint reached `continue` will continue script. Anything typed before that will be executed.")
+	fmt.Println("breakpoint reached `continue` will continue script. Anything typed before that will be executed.\nStack:")
+	{ // print stack trace
+		var b bytes.Buffer
+		stack := rt.CaptureCallStack(0, nil)
+		for _, frame := range stack {
+			b.WriteRune('\t')
+			frame.Write(&b)
+			b.WriteRune('\n')
+		}
+		fmt.Println(b.String())
+	}
 	for {
 		fmt.Print("`continue` will continue script-> ")
 		text, _ := reader.ReadString('\n')
